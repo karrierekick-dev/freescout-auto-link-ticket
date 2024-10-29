df09fd3cc46dbb30e98631618e8d9fe410fabb23
Karriere Kick Entwickler <97171654+karrierekick-dev@users.noreply.github.com>
2024-10-29 15:08:33 +0100

    Delete Http directory

--- Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => \Helper::getSubdirectory(), 'namespace' => 'Modules\AutoLinkTicket\Http\Controllers'], function()
-{
-    Route::get('/', 'AutoLinkTicketController@index');
-});
